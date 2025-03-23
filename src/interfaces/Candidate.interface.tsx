96e441aadbf30bcddd32d3c66898656b73a77dbe
nolan MARSHKE <nmarshke@gmail.com>
2025-03-23 14:16:51 -0400

    added interface

--- src/interfaces/Candidate.interface.tsx
+++ src/interfaces/Candidate.interface.tsx
@@ -1,1 +1,11 @@
 // TODO: Create an interface for the Candidate objects returned by the API
+export interface Candidate {
+    id: number;
+    login: string; // GitHub username
+    name: string;
+    avatar_url: string;
+    location: string | null;
+    email: string | null;
+    html_url: string;
+    company: string | null;
+  } 
